5b371264d39ce2d5b4212a9fd6954b9815c57a1a
Tomasz Naklicki <tnaklicki01@gmail.com>
2024-06-14 12:31:15 +0200

    removed commented code

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ if __name__ == "__main__":
 
     mpc = MPC(data=data, trajectory_id=0)
 
-    # Create MPC model
-    # mpc_model = create_model(data)
-    # mpc = create_mpc(mpc_model)
-
-    # sim = simulate_mpc_mujoco(mpc, panda, data)
-
     # Plot results
     joint_states = mpc.simulate()
     t = np.arange(len(joint_states[1])) * panda.opt.timestep
@@ -23,7 +17,6 @@ if __name__ == "__main__":
     plt.figure()
     for i in range(7):
         plt.plot(t, joint_states[i + 1], label=f"Joint {i+1}")
-    # plt.plot(t, get_trajectory(), label="trajectory")
     plt.xlabel("Time [s]")
     plt.ylabel("Joint position [rad]")
     plt.legend()
